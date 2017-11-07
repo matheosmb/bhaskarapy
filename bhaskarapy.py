5628fc9699383d4b527623dac0eae5d638050123
matheosmb <33439468+matheosmb@users.noreply.github.com>
2017-11-07 17:58:59 -0300

    Update bhaskarapy.py

--- bhaskarapy.py
+++ bhaskarapy.py
@@ -1,23 +1,28 @@
-def entry():
-	a = input ("Type the first coefficient")
-	b = input ("Type the second coefficent")
-	c = input ("Type the third coefficient")
-	return a,b,c
+import math;
 
-def calculation ():
-	#Since we are going to use sqrt, to make things easier we import the math library before
-	from math import sqrt
-	variables = entry()	#We are receiving the parameters from entry function and storing to variables
-	delta = (variables[1]**2 - 4*variables[0]*variables[2])
+class bhaskara():
+	a = int(input("Type the first coefficient"));
+	b = int(input("Type the second coefficent"));
+	c = int(input("Type the third coefficient"));
 
-	while (delta  < 0): 
-		print ("This algorithm does not support imaginary roots")
-		variables = entry()	#One more time, we call the function entry and store to variables again.
-		delta = (variables[1]**2 - 4*variables[0]*variables[2])
+	def calculation(self):
+		delta = (self.b**2-4*self.a*self.c);
+		print(delta)
 
-	if (delta >= 0):
-		x1 = (-variables[1] + sqrt(delta))/(2*variables[0])
-		x2 = (-variables[1] - sqrt(delta))/(2*variables[0])
-		print ("The first root is: "), x1
-		print ("The second root is: "), x2
-calculation() 		
+		if(delta < 0):
+			print ("This algorithm does not support imaginary roots")
+			variables = self.entry()	#One more time, we call the function entry and store to variables again.
+			delta = (variables[1]**2 - 4*variables[0]*variables[2])
+
+		elif (delta > 0):
+			x1 = (-self.b+math.sqrt(delta))/(2*self.a);
+			x2 = (-self.b-math.sqrt(delta))/(2*self.a);
+			print ("The first root is: {}" .format(x1));
+			print ("The second root is: {}" .format(x2));
+
+		else:
+			x1 = (-self.b+math.sqrt(delta))/(2*self.a);
+			print("The only root is: {}" .format(x1));
+			
+b = bhaskara();
+b.calculation();
